21f2d4019cb00aae18330d0341dff80ba5e4a5e8
DineshJWA <118792155+DineshJWA@users.noreply.github.com>
2023-03-16 12:59:57 +0530

    Updates

---
+++ precache-manifest.483c4f2db2488a5952e1d3a11a8be69c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06045f694f80b6526f89af4c6a3f189e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf4484f257980feaea0e",
+    "url": "/static/css/2.beb3506c.chunk.css"
+  },
+  {
+    "revision": "7d72b9bd38cd1ee952c4",
+    "url": "/static/css/main.daa62b35.chunk.css"
+  },
+  {
+    "revision": "cf4484f257980feaea0e",
+    "url": "/static/js/2.22b3486f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.22b3486f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d72b9bd38cd1ee952c4",
+    "url": "/static/js/main.48ada623.chunk.js"
+  },
+  {
+    "revision": "d0d619bd6016fbbf1f63",
+    "url": "/static/js/runtime-main.7dda7149.js"
+  },
+  {
+    "revision": "6b09b0f6bd575dad3bfe952e8555f3d4",
+    "url": "/static/media/img2.6b09b0f6.jpg"
+  },
+  {
+    "revision": "6edc43496d9d199cd28ad6c6b2bfcad5",
+    "url": "/static/media/imgbackground.6edc4349.jpg"
+  },
+  {
+    "revision": "8321431e5fb3c5425696cce934161beb",
+    "url": "/static/media/upadhya.8321431e.png"
+  }
+]);
